a5be3fc69f2853481b9368cfd8497f2261ae37d9
KEVIN THOMAS <kevinthomas1114@gmail.com>
2024-12-17 21:05:08 +0530

    updated file

--- ecom/settings.py
+++ ecom/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ecom2.urls'
+ROOT_URLCONF = 'ecom.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ecom2.wsgi.application'
+WSGI_APPLICATION = 'ecom.wsgi.application'
 
 
 # Database
